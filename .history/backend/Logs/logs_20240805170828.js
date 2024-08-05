3c1ec2f3e30199a60267ece5db52f08c26c73f89
Shashank <97978650+shash0708@users.noreply.github.com>
2024-08-05 17:09:03 +0530

    ok

--- .history/backend/Logs/logs_20240805170828.js
+++ .history/backend/Logs/logs_20240805170828.js
@@ -847,7 +847,7 @@ router.post('/pdf', fetchuser, async (req, res) => {
     // Launch Puppeteer and generate the PDF
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true
+      
     });
     const page = await browser.newPage();
     await page.setContent(html);
